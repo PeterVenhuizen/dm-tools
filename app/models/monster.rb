36cb322730d2e9b904cd2e0a181048cc0d5758d0
Peter Venhuizen <peter_venhuizen@hotmail.com>
2022-03-13 17:17:55 +0100

    Add in first tests and validations

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 class Monster < ApplicationRecord
+  validates :challenge_rating,
+            inclusion: {
+              in: %w[1/8 1/4 1/2] + [*'0'..'30'],
+              message: '%{value} is not a valid challenge rating'
+            }
+
+  validates :name,
+            presence: {
+              minimum: 2,
+              message: 'must be at least 2 characters long'
+            }
+
+  validates :size,
+            inclusion: { in: %w[Tiny Small Medium Large Huge Gargantuan] }
+
+  validates :xp,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0
+            }
 end
--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Monster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    bandit = build(:bandit)
+    expect(bandit).to be_valid
+  end
+
+  it 'has a name at least 2 characters long' do
+    bandit = build(:bandit, name: '')
+    expect(bandit).not_to be_valid
+    expect(bandit.errors.messages[:name]).not_to be_empty
+  end
+
+  it 'has a default challenge rating of zero' do
+    monster = Monster.new
+    expect(monster.challenge_rating).to eq '0'
+  end
+
+  describe 'challenge_rating' do
+    it 'is positive' do
+      bandit = build(:bandit, challenge_rating: -1)
+      expect(bandit).not_to be_valid
+    end
+
+    it 'is not bigger than 30' do
+      bandit = build(:bandit, challenge_rating: 31)
+      expect(bandit).not_to be_valid
+    end
+  end
+
+  describe 'xp' do
+    it 'is numeric' do
+      bandit = build(:bandit, xp: 'abc')
+      expect(bandit).not_to be_valid
+    end
+
+    it 'is positive' do
+      bandit = build(:bandit, xp: -31)
+      expect(bandit).not_to be_valid
+    end
+  end
+
+  describe 'size' do
+    it 'must have a size' do
+      bandit = build(:bandit, size: nil)
+      expect(bandit).not_to be_valid
+    end
+
+    it 'can be Tiny, Small, Medium, Large, Huge, Gargantuan' do
+      sizes = %w[Tiny Small Medium Large Huge Gargantuan]
+      sizes.each do |size|
+        bandit = build(:bandit, size: size)
+        expect(bandit).to be_valid
+      end
+    end
+
+    it 'cannot be a Giant' do
+      giant_bandit = build(:bandit, size: 'Giant')
+      expect(giant_bandit).not_to be_valid
+    end
+  end
 end
