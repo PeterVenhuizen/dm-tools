d8c6098b9c67907ad8c0935e628b4d87084382c6
Peter Venhuizen <peter_venhuizen@hotmail.com>
2022-03-13 16:41:33 +0100

    Add monster factories

--- spec/factories/monsters.rb
+++ spec/factories/monsters.rb
@@ -1,17 +1,92 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :monster do
-    name { "MyString" }
-    size { "MyString" }
-    species { "MyString" }
-    armor_class { "MyString" }
-    hit_points { 1 }
-    challenge_rating { "MyString" }
-    alignment { "MyString" }
-    hit_dice { "MyString" }
-    ability_score { "" }
-    xp { 1 }
-    proficiency_bonus { 1 }
-    speed { "" }
-    languages { "MyString" }
+  factory :bandit, class: 'Monster' do
+    name { 'Bandit' }
+    size { 'Medium' }
+    species { 'Humanoid' }
+    alignment { 'any non-lowful alignment' }
+    languages { 'Any one language (usually Common)' }
+    armor_class { '12 (leather armor)' }
+    hit_points { 11 }
+    hit_dice { '2d8 + 2' }
+    ability_score do
+      {
+        strength: 11,
+        dexterity: 12,
+        constitution: 12,
+        intelligence: 10,
+        wisdom: 10,
+        charisma: 10
+      }
+    end
+    challenge_rating { '1/8' }
+    xp { 25 }
+    proficiency_bonus { 2 }
+    speed do
+      {
+        walk: 30
+      }
+    end
+  end
+
+  factory :cat, class: 'Monster' do
+    name { 'Cat' }
+    size { 'Tiny' }
+    species { 'Beast' }
+    alignment { 'unaligned' }
+    languages { '' }
+    armor_class { '12' }
+    hit_points { 2 }
+    hit_dice { '1d4' }
+    ability_score do
+      {
+        strength: 3,
+        dexterity: 15,
+        constitution: 10,
+        intelligence: 3,
+        wisdom: 12,
+        charisma: 7
+      }
+    end
+    challenge_rating { '0' }
+    xp { 10 }
+    proficiency_bonus { 2 }
+    speed do
+      {
+        climb: 30,
+        walk: 40
+      }
+    end
+  end
+
+  factory :cat, class: 'Monster' do
+    name { 'Harpy' }
+    size { 'Medium' }
+    species { 'Beast' }
+    alignment { 'chaotic evil' }
+    languages { 'Common' }
+    armor_class { '11' }
+    hit_points { 38 }
+    hit_dice { '7d8 + 7' }
+    ability_score do
+      {
+        strength: 12,
+        dexterity: 13,
+        constitution: 12,
+        intelligence: 7,
+        wisdom: 10,
+        charisma: 13
+      }
+    end
+    challenge_rating { '1' }
+    xp { 200 }
+    proficiency_bonus { 2 }
+    speed do
+      {
+        fly: 40,
+        walk: 20
+      }
+    end
   end
 end
